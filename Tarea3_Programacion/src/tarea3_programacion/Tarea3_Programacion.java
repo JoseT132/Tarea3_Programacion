9651d1d723940e60b318315eeb38d60994b57381
JoseT132 <140220944+JoseT132@users.noreply.github.com>
2024-04-04 22:33:10 -0600

    Eliminacion

--- Tarea3_Programacion/src/tarea3_programacion/Tarea3_Programacion.java
+++ Tarea3_Programacion/src/tarea3_programacion/Tarea3_Programacion.java
@@ -1,20 +1,12 @@
-/*
- * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
- * Click nbfs://nbhost/SystemFileSystem/Templates/Classes/Main.java to edit this template
- */
+
 package tarea3_programacion;
 
-/**
- *
- * @author josed
- */
+
 public class Tarea3_Programacion {
 
-    /**
-     * @param args the command line arguments
-     */
+   
     public static void main(String[] args) {
-        // TODO code application logic here
+        
     }
     
 }
